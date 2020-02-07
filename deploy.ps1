d22ea62f52ea0885e406d8117dae3cd4a909b277
Ryan Ephgrave <ryan2065@gmail.com>
2020-02-07 15:01:17 -0600

    updated name, added deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -5,9 +5,10 @@ Param(
     [string]$ClientSecret,
     [Parameter(Mandatory=$true)]
     [ValidateSet("Intune", "ConfigMgr")]
-    [string]$Connector
+    [string]$Connector,
+    [switch]$Update
 )
-
+$CurrentLocation = Get-Location
 Set-Location "$PSScriptRoot\$Connector"
 
 $apiPropertiesContent = Get-Content "apiProperties.json" -Raw
@@ -17,8 +18,24 @@ $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
 Remove-Item apiProperties.json -Force -ErrorAction SilentlyContinue
 [System.IO.File]::WriteAllLines("$PSScriptRoot\$Connector\apiProperties.json", $correctAPIPropertiesContent, $Utf8NoBomEncoding)
 
-paconn create --api-def apiDefinition.swagger.json --api-prop apiProperties.json --secret "$ClientSecret"
+$RanCommand = $false
+if(Get-Command 'paconn.exe' -ErrorAction SilentlyContinue){
+    $RanCommand = $true
+    if($update){
+        paconn update --api-def apiDefinition.swagger.json --api-prop apiProperties.json --secret "$ClientSecret"
+    }
+    else {
+        paconn create --api-def apiDefinition.swagger.json --api-prop apiProperties.json --secret "$ClientSecret"
+    }
+}
+
 
 #cleanup
 Remove-Item apiProperties.json -Force -ErrorAction SilentlyContinue
-[System.IO.File]::WriteAllLines("$PSScriptRoot\$Connector\apiProperties.json", $apiPropertiesContent, $Utf8NoBomEncoding)
+[System.IO.File]::WriteAllLines("$PSScriptRoot\$Connector\apiProperties.json", $apiPropertiesContent.Trim(), $Utf8NoBomEncoding)
+
+if($false -eq $RanCommand){
+    throw "Paconn is not installed. This will require Python be installed on this computer. Please see ReadMe for instructions. Paconn documentation located at: https://pypi.org/project/paconn/"
+}
+
+Set-Location $CurrentLocation
